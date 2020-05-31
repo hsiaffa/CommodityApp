d32a7fa42e4874274ea2a7138dace811d93333fd
Hsiaffa <hiltonsiaffa@gmail.com>
2020-05-31 19:37:51 -0400

    Add project files.

---
+++ CommodityApp/CommodityItem.cs
@@ -0,0 +1,127 @@
+﻿//Grading ID here
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommodityApp
+{
+    public class CommodityItem
+    {
+        private string _description;
+        private string _productnumber;
+        private int _quantity;
+        private double _price;
+        // #1 - Put needed backing fields here
+        // Remember: Some properties may be
+        // auto-implemented, so only create
+        // backing fields for ones needed
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+            }
+            // #2 - Put your code here for get/set accessors
+            // May be auto-implemented
+
+        }
+
+        public string ProductNumber
+        {
+            get
+            {
+                return _productnumber;
+            }
+            set
+            {
+                _productnumber = value;
+            }
+            // #3 - Put your code here for get/set accessors
+            // May be auto-implemented
+
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return _quantity;
+            }
+            set
+            {
+                if (value > 0)
+                    _quantity = value;
+                else
+                    _quantity = 0;
+            }
+            // #4 - Put your code here for get/set accessors
+            // Set must validate!
+
+        }
+
+        public double Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value > 0)
+                    _price = value;
+                else
+                _price = 0;
+            }
+            // #5 - Put your code here for get/set accessors
+            // Set must validate!
+
+        } 
+
+        public CommodityItem(string desc, string productNum, int quant, double price)
+        {
+            Description = desc;
+            ProductNumber = productNum;
+            Quantity = quant;
+            Price = price;
+            // #6 - Put your code here for constructor
+            // Be sure your parameters get validated through properties
+
+        }
+
+        public double CalcValue()
+        {
+
+            
+            double total;
+
+           total =  Quantity * Price;
+            return total;
+            // #7 - Put your code here to calculate value of
+            // this item in inventory
+            // Example: 2000 @ $9.99 = $19,980
+
+        }
+
+        public override string ToString()
+        {
+
+            return $"{ProductNumber} {Description} ({Quantity} {Price:C})";
+            // #8 - Put your code here to create formatted string
+            // Use format that would produce a string like:
+            // "TP1234: Toilet Papers (2500 @ $29.99)"
+            // where TP1234 is the prodcut number, Toilet Paper is the
+            // description, 2500 is the quantity, and $29.99 is
+            // the price. No newlines are needed.
+
+
+        }
+    }
+}
---
+++ CommodityApp/Program.cs
@@ -0,0 +1,49 @@
+﻿// Grading ID Here
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CommodityApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentValue;   // Current commodity's value
+            double totalValue = 0; // Running total of commodity's value
+            CommodityItem[] items; // Commodity Items
+
+            // #1 - Put code here to construct 3  commodity items: Toilet Paper, Hand Sanitizer, Disinfection Wipes as directed
+          
+
+            CommodityItem item1 = new CommodityItem("Sonia Toilet Paper", "TP6565", 10, 6.50);  //Toilet Paper
+            CommodityItem item2 = new CommodityItem("LIB Hand Sanitizer", "HS8985", 13, 7.50); //Hand Sanitizer
+            CommodityItem item3 = new CommodityItem("Lysol Disinfection Wipes", "DW9494", 10, 6.50); //Disinfection Wipes
+
+            items = new CommodityItem[] { item1, item2, item3 };
+
+            WriteLine($"Commodity Revenue Report:");
+
+            // #2 - Complete loop to step through array of items
+            for (int index = 0; index < items.Length; ++index)
+            {
+
+                currentValue = items[index].CalcValue();  //      from array by calling CalcValue
+
+                totalValue += currentValue; // #4 - Add currentValue into running total
+
+                WriteLine($"{currentValue:C}, {items[index].ToString()}");
+                // #5 - Output currentValue, with currency formatting, followed by string representation
+                //      of current item from array
+            }
+
+            WriteLine($"\nTotal Gross Revenue:{totalValue:C}");
+            // #6 - Output totalValue of Gross Revenue, with currency formatting
+           
+        }
+    }
+}
